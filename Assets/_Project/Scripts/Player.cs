2b930347277767bd934deab6e16a369e53d32c56
Anderson Paim <andersonpaim15@outlook.com>
2022-08-02 17:44:06 -0300

    Started player movement

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -4,19 +4,50 @@ namespace _Project.Scripts
 {
     public class Player : MonoBehaviour
     {
+        private Rigidbody _rb;
+        private bool _isGrounded = true;
+        
+        public void OnStart()
+        {
+            Initialize();
+        }
+
+        public void OnUpdate()
+        {
+            GroundCheck();
+        }
+        
         public void Move(Vector2 moveDirection)
         {
-            Debug.Log("MOVE: " + moveDirection);
+            moveDirection *= 8; //TODO GET PLAYER SPEED
+            Vector3 rightDirection = transform.right;
+            Vector3 playerDirection = new Vector3(-rightDirection.z, 0, rightDirection.x);
+            Vector3 movementDirection = (playerDirection * moveDirection.y + rightDirection * moveDirection.x);
+            movementDirection.y = _rb.velocity.y;
+            _rb.velocity = movementDirection;
         }
         
         public void Jump()
         {
-            Debug.Log("JUMP");
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            }
         }
         
         public void Interact()
         {
             Debug.Log("INTERACT");
+        }
+
+        private void Initialize()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+        
+        private void GroundCheck()
+        {
+            _isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.3f);
         }
     }
 }
--- Assets/_Project/Scripts/System/GameplaySystem.cs
+++ Assets/_Project/Scripts/System/GameplaySystem.cs
@@ -6,15 +6,18 @@ namespace _Project.Scripts.System
     public class GameplaySystem : MonoBehaviour
     {
         [SerializeField] private InputManager _inputManager;
+        [SerializeField] private Player _player;
         
         private void Start()
         {
             _inputManager.OnStart();
+            _player.OnStart();
         }
 
         private void Update()
         {
             _inputManager.OnUpdate();
+            _player.OnUpdate();
         }
     }
 }
