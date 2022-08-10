4862e5f41693640a5b819049994d4baeaf314441
Anderson Paim <andersonpaim15@outlook.com>
2022-08-10 16:54:23 -0300

    Fixed input manager bug after changing a scene

--- Assets/_Project/Scripts/Manager/InputManager.cs
+++ Assets/_Project/Scripts/Manager/InputManager.cs
@@ -2,7 +2,9 @@ using System;
 using _Project.Scripts.Events;
 using Coimbra.Services;
 using Coimbra.Services.Events;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Project.Scripts.Manager
 {
@@ -25,34 +27,54 @@ namespace _Project.Scripts.Manager
             Movement();
         }
 
+        private void OnDestroy()
+        {
+            DestroyEvents();
+        }
+
         private void SetupEvents()
         {
             _input = new PlayerInputActions();
-            _input.Player.Jump.performed += _ => Jump();
-            _input.Player.Grab.performed += _ => Grab();
-            _input.Player.Crouch.performed += _ => Crourch(true);
-            _input.Player.Crouch.canceled += _ => Crourch(false);
+            _input.Player.Jump.performed += Jump;
+            _input.Player.Grab.performed += Grab;
+            _input.Player.Crouch.performed += Crourch;
+            _input.Player.Crouch.canceled += Crourch;
         }
 
+        private void DestroyEvents()
+        {
+            _input.Player.Jump.performed -= Jump;
+            _input.Player.Grab.performed -= Grab;
+            _input.Player.Crouch.performed -= Crourch;
+            _input.Player.Crouch.canceled -= Crourch;
+        }
+        
         private void Movement()
         {
             _player.Move(_input.Player.Movement.ReadValue<Vector2>());
         }
 
-        private void Jump()
+        private void Jump(InputAction.CallbackContext ctx)
         {
             _player.Jump();
         }
 
-        private void Grab()
+        private void Grab(InputAction.CallbackContext ctx)
         {
             OnGrab grab = new OnGrab();
             grab?.Invoke(_eventService);
         }
         
-        private void Crourch(bool isCrouching)
+        private void Crourch(InputAction.CallbackContext ctx)
         {
-            _player.Crouch(isCrouching);
+            if (ctx.performed)
+            {
+                _player.Crouch(true);
+            }
+            else
+            {
+                _player.Crouch(false);
+            }
         }
     }
 }
